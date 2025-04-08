a584f58e38aea7dab690732cf839acc0a0104503
Wonbin Jin <wbjin@umich.edu>
2025-04-08 12:25:11 -0400

    Update util script

--- util.sh
+++ util.sh
@@ -7,16 +7,21 @@ if [ -z "$PROCESS_NAME" ]; then
     exit 1
 fi
 
-echo "🔍 Monitoring process '$PROCESS_NAME' every 60 seconds..."
+# Color codes
+CYAN_BOLD="\033[1;36m"
+YELLOW="\033[0;33m"
+RESET="\033[0m"
+
+echo -e "🔍 Monitoring process '$PROCESS_NAME' every 60 seconds..."
 
 while true; do
     # Get number of input files and add 1
     NUM_FILES=$(find ~/index/input -type f | wc -l)
     ARG=$((NUM_FILES + 1))
-    echo "📂 Number of input files: $NUM_FILES"
+    echo -e "📂 ${CYAN_BOLD}Number of input files: $NUM_FILES${RESET}"
 
-    # Show disk usage
-    df -h ~
+    # Show disk usage in yellow
+    echo -e "${YELLOW}$(df -h ~)${RESET}"
 
     # Check if process is running
     if pgrep -x "$PROCESS_NAME" > /dev/null; then
@@ -30,3 +35,4 @@ while true; do
 
     sleep 60
 done
+
